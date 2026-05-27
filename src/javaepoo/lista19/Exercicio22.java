c18e1a6691fcdbb642ce4706639020d988bdb738
Manoel Max <manoelmaxcl@gmail.com>
2026-05-27 15:42:16 -0300

    Alteração no arquivo Exercicio22.java

--- src/javaepoo/lista19/Exercicio22.java
+++ src/javaepoo/lista19/Exercicio22.java
@@ -1,3 +1,9 @@
+/**
+ * Gerar aleatoriamente um vetor A com 10 elementos inteiros e iguais a
+ * 0 e 1. Pede-se para implementar um programa que determine o percentual de
+ * números 0's e 1's existentes no vetor A.
+ */
+
 package javaepoo.lista19;
 
 public class Exercicio22 {
